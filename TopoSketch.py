3a6331e1d181e856daa9b09d07d4252ce2e526dc
“Salaar <salaar@seas.upenn.edu>
2018-04-28 16:44:17 -0400

    Added patch merge function

--- TopoSketch.py
+++ TopoSketch.py
@@ -2,6 +2,28 @@ import maya.mel as mel
 import maya.cmds as cmds
 import math
 
+def mergePatches():
+	patches = []
+	for o in mel.eval("ls -et transform;"):
+	    if o.startswith("boundary"):
+	        patches.append(str(o))
+	if len(patches) > 1:
+		# Merge patches 1 by 1 by selecting boundary edges and using polySewEdge
+		cmds.rename(patches[0], "boundaryMerged0")
+		for p in range(1, len(patches)):
+		    edge = cmds.polyEvaluate("boundaryMerged"+str(p-1), e=True)
+		    cmds.polyUnite("boundaryMerged"+str(p-1), patches[p], n="boundaryMerged"+str(p))
+		    cmds.polySelect("boundaryMerged"+str(p), eb=0)
+		    cmds.polySelect("boundaryMerged"+str(p), eb=edge, add=True)
+		    sewcmd = "polySewEdge -t 1 -tx 1 -ws 1 -ch 1 "
+		    for e in mel.eval("ls -sl"):
+		    	sewcmd += e+" "
+		    sewcmd += ";"
+		    print(sewcmd)
+		    mel.eval(sewcmd)
+	
+mergePatches()
+
 def curveInt(curve1, curve2, tol=0.2, num=45):
 	for a in range(0, num):
 		for b in range(0, num):
@@ -52,16 +74,18 @@ def checkLoops():
 					rv.append(loop)
 	return rv
 
-objs = mel.eval("ls -et transform;")
-
 curves = {}
+def makePatches():
+	objs = mel.eval("ls -et transform;")
 
-for o in objs:
-    if o[0:5] == "curve":
-        newCurve(o)
+	for o in objs:
+	    if o[0:5] == "curve":
+	        newCurve(o)
 
-for loop in checkLoops():
-	loop = list(loop)
-	print("Loop: "+str(loop))
-	# Make surface patch
-	cmds.boundary(loop[0], loop[1], loop[2], loop[3], order=False)
+	for loop in checkLoops():
+		loop = list(loop)
+		print("Loop: "+str(loop))
+		# Make surface patch
+		cmds.boundary(loop[0], loop[1], loop[2], loop[3], order=False)
+
+#makePatches()
